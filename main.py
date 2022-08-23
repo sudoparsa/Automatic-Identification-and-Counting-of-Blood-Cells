6c099639611226bdbdcabbefb8d6595887d72692
sudoparsa <sp.hoseiny@gmail.com>
2022-08-23 17:35:26 +0430

    add feature different zoom

--- main.py
+++ main.py
@@ -1,112 +1,85 @@
 import cv2
-import time
-from utils import iou
-from scipy import spatial
 from darkflow.net.build import TFNet
 import sys
+from PIL import Image
+from tqdm import tqdm
+
 
 options = {'model': 'cfg/tiny-yolo-voc-3c.cfg',
            'load': 3750,
-           'threshold': 0.1,
+           'threshold': 0.15,
            'gpu': 0.7}
 
 tfnet = TFNet(options)
 
-pred_bb = []  # predicted bounding box
-pred_cls = []  # predicted class
-pred_conf = []  # predicted class confidence
 
+if len(sys.argv) > 1:
+    im_name = sys.argv[1]
+else:
+    im_name = 'data/HRI001.jpg'
 
-def blood_cell_count(file_name):
-    rbc = 0
-    wbc = 0
-    platelets = 0
-
-    cell = []
-    cls = []
+def predict(im_name, out_name='outh.jpg', alpha=5):
+    C = []  # Center
+    R = []  # Radius
+    L = []  # Label
     conf = []
 
+    img = Image.open(im_name)
+    width, height = int(img.size[0] * alpha), int(img.size[1] * alpha)
+    img = img.resize(width, height)
+    temp = 'temp.jpg'
+    img.save(temp)
+
+    image = cv2.imread(temp, -1)
+    for h in range(0, height, 480):
+        for w in range(0, width, 640):
+            im = image[h:h + 480, w:w + 640]
+            output = tfnet.return_predict(im)
+
+            RBC = 0
+            WBC = 0
+            Platelets = 0
+
+            for prediction in output:
+                label = prediction['label']
+                confidence = prediction['confidence']
+
+                tl = (prediction['topleft']['x'], prediction['topleft']['y'])
+                br = (prediction['bottomright']['x'], prediction['bottomright']['y'])
+
+                height, width, _ = image.shape
+                center_x = int((tl[0] + br[0]) / 2)
+                center_y = int((tl[1] + br[1]) / 2)
+                center = (center_x + w, center_y + h)
+                radius = int((br[0] - tl[0]) / 2)
+
+                C.append(center)
+                R.append(radius)
+                L.append(label)
+                conf.append(confidence)
+
     record = []
-    tl_ = []
-    br_ = []
-    iou_ = []
-    iou_value = 0
 
-    tic = time.time()
-    image = cv2.imread(file_name, -1)
-    output = tfnet.return_predict(image)
-
-    for prediction in output:
-        label = prediction['label']
-        confidence = prediction['confidence']
-        tl = (prediction['topleft']['x'], prediction['topleft']['y'])
-        br = (prediction['bottomright']['x'], prediction['bottomright']['y'])
-
-        if label == 'RBC' and confidence < .125:
-            continue
-        if label == 'WBC' and confidence < .0625:
-            continue
-        if label == 'Platelets' and confidence < .0625:
-            continue
-
-        # clearing up overlapped same platelets
-        if label == 'Platelets':
-            if record:
-                tree = spatial.cKDTree(record)
-                index = tree.query(tl)[1]
-                iou_value = iou(tl + br, tl_[index] + br_[index])
-                iou_.append(iou_value)
-
-            if iou_value > 0.1:
-                continue
-
-            record.append(tl)
-            tl_.append(tl)
-            br_.append(br)
-
-        center_x = int((tl[0] + br[0]) / 2)
-        center_y = int((tl[1] + br[1]) / 2)
-        center = (center_x, center_y)
+    for i in range(0, len(C)):
+        center = C[i]
+        radius = R[i]
+        label = L[i]
+        confidence = conf[i]
 
         if label == 'RBC':
             color = (255, 0, 0)
-            rbc = rbc + 1
-        if label == 'WBC':
+        elif label == 'WBC':
             color = (0, 255, 0)
-            wbc = wbc + 1
-        if label == 'Platelets':
+        elif label == 'Platelets':
             color = (0, 0, 255)
-            platelets = platelets + 1
 
-        radius = int((br[0] - tl[0]) / 2)
-        image = cv2.circle(image, center, radius, color, 2)
+        image = cv2.circle(image, center, radius, color, 5)
         font = cv2.FONT_HERSHEY_COMPLEX
-        image = cv2.putText(image, label + ' ' + str(confidence), (center_x - 15, center_y + 5), font, .5, color, 1)
-        cell.append([tl[0], tl[1], br[0], br[1]])
+        image = cv2.putText(image, str(confidence), (center[0] - 30, center[1] + 10), font, 1, color, 2)
 
-        if label == 'RBC':
-            cls.append(0)
-        if label == 'WBC':
-            cls.append(1)
-        if label == 'Platelets':
-            cls.append(2)
+    cv2.imwrite(out_name, image)
 
-        conf.append(confidence)
 
-    toc = time.time()
-    pred_bb.append(cell)
-    pred_cls.append(cls)
-    pred_conf.append(conf)
-    avg_time = (toc - tic) * 1000
-    print('{0:.5}'.format(avg_time), 'ms')
-
-    cv2.imwrite('out.jpg', image)
-
-if len(sys.argv) > 1:
-    image_path = sys.argv[1]
-else:
-    image_path = 'data/image_001.jpg'
-
-blood_cell_count(image_path)
-
-print('All Done!')
+alpha_list = [4.75, 5, 5.25, 5.5]
+for alpha in tqdm(alpha_list):
+    predict(im_name, out_name=f'out_{alpha}.jpg', alpha=alpha)
